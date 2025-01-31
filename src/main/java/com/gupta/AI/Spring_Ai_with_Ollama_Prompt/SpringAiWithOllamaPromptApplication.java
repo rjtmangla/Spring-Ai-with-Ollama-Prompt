21174b1aa34e76e3328f4832874d8ed41db30a9c
Rajat Gupta <rjt.mangla@gmail.com>
2025-01-31 22:52:58 +0530

    init

---
+++ src/main/java/com/gupta/AI/Spring_Ai_with_Ollama_Prompt/SpringAiWithOllamaPromptApplication.java
@@ -0,0 +1,13 @@
+package com.gupta.AI.Spring_Ai_with_Ollama_Prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiWithOllamaPromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiWithOllamaPromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gupta/AI/Spring_Ai_with_Ollama_Prompt/SpringAiWithOllamaPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gupta.AI.Spring_Ai_with_Ollama_Prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiWithOllamaPromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
